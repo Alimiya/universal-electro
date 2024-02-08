1ec44f4835182c50874263e77095421d75d84c4e
aalohaa <khusainov.alizhan@bk.ru>
2024-02-08 11:44:26 +0600

    nodemon installed & comments removed

--- server.js
+++ server.js
@@ -4,23 +4,19 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const port = 3000;
 
-// -------- routes --------
 const index = require('./routes/index');
 
-//html ejs
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-// -- used packages --
 app.use('/views', express.static(path.join(__dirname + '/views')));
 app.use('/public', express.static(path.join(__dirname + '/public')));
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-// -- routes --
 app.use('/', index);
-
 
 app.listen(port, ()=> {
     console.log(`app listening at port http://localhost:${port}`);
