7987b8576a8abab480a30e81f6b106802181bcdc
cnrprod-team-50452 <1690610961@codenrock.com>
2024-02-12 03:07:03 +0600

    updated catalogController

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -5,22 +5,26 @@ const fdb = require('../firebase').fdb;
 exports.getProducts = async function (req, res, next) {
     let data = [];
 
-    await fdb.collection('products').get().then((products)=> {
-        products.forEach((product) => {
-            var product_data = {
-                product_id: product.id,
-                title: product.data().title,
-                description: product.data().description,
-                price: product.data().price,
-                category: product.data().category,
-                status: product.data().status,
-                quantity: product.data().quantity,
-                product_img: product.data().product_img,
-            };
-            data.push(product_data);
+    try {
+        await fdb.collection('products').get().then((products) => {
+            products.forEach((product) => {
+                const product_data = {
+                    product_id: product.id,
+                    title: product.data().title,
+                    description: product.data().description,
+                    price: product.data().price,
+                    category: product.data().category,
+                    status: product.data().status,
+                    quantity: product.data().quantity,
+                    product_img: product.data().product_img,
+                };
+                data.push(product_data);
+            });
         });
-    });
-    res.send(data);
+        res.send(data);
+    } catch (err) {
+        console.log(err)
+    }
 }
 // GET /:id - filter: title, description, img, price, category, status, quantity
 
