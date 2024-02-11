6a230e9e7e861d71ae6ed6ccebdcbba487e16fe2
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-02-12 02:29:04 +0600

    login finished

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,38 +4,29 @@ const fauth = require('../firebase').fauth;
 
 exports.login = async function (req, res, next) {
     let r = { r: 0 };
-    const {email, password} =req.body
-    console.log(email)
-    console.log(password)
-
+    const {email, password} = req.body;
+    
     try {
+
         await fauth.signInWithEmailAndPassword(fauth.getAuth(), email, password).then((userCredential) => {
+          console.log(userCredential)
             const token = generateAdminToken(userCredential)
-            console.log(token)
+            console.log(userCredential.user);
             res.cookie('admin', token, {maxAge: process.env.TOKEN_EXPIRE * 100000})
             res.header('Authorization', `Bearer ${token}`)
             r['r'] = 1;
-            res.json({token})
-        }).catch((e) => {
-            console.log(e);
-
-            if (e.code == 'auth/invalid-email') {
-                r['r'] = 2;
+            res.send(r)
+        }, (err) => {
+            console.log(err);
+            if (err.code == 'auth/user-not-found') {
+              r['r'] = 2;
+            } else if (err.code == 'auth/wrong-password') {
+              r['r'] = 0;
+            } else if (err.code == 'auth/too-many-requests') {
+              r['r'] = 3;
             }
-            ;
-            if (e.code == 'auth/missing-password') {
-                r['r'] = 3;
-            }
-            ;
-            if (e.code == 'auth/invalid-credential') {
-                r['r'] = 4;
-            }
-            ;
-            if (e.code == 'auth/too-many-requests') {
-                r['r'] = 5
-            }
-            res.send(r);
-        })
+            res.send(JSON.stringify(r));
+          });
     } catch (err) {
         console.log(err);
         r['r'] = 6;
