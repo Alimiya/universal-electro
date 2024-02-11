531a25f0acb9549b97c1ce7f173d52681ef04379
aalohaa <khusainov.alizhan@bk.ru>
2024-02-12 03:05:07 +0600

    get products

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,4 +1,27 @@
-// GET / - filter: title, description, img, price, category, status, quantity
+// GET / - filter: title, description, img, price, category, status, quantity=
+
+const fdb = require('../firebase').fdb;
+
+exports.getProducts = async function (req, res, next) {
+    let data = [];
+
+    await fdb.collection('products').get().then((products)=> {
+        products.forEach((product) => {
+            var product_data = {
+                product_id: product.id,
+                title: product.data().title,
+                description: product.data().description,
+                price: product.data().price,
+                category: product.data().category,
+                status: product.data().status,
+                quantity: product.data().quantity,
+                product_img: product.data().product_img,
+            };
+            data.push(product_data);
+        });
+    });
+    res.send(data);
+}
 // GET /:id - filter: title, description, img, price, category, status, quantity
 
 // POST / - сделать запрос на покупку корзины с покрытием в пдф
--- routes/catalogRoute.js
+++ routes/catalogRoute.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const Controller = require('../controllers/catalogController')
+
+router.get('/products', Controller.getProducts);
 
 module.exports = router
