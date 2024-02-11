89c44bb4b3c93c037cf4008d1ea6157caa2f2281
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-02-12 00:46:27 +0600

    index.js modified

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,18 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index');
-})
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/admin/products', (req, res) => {
+    res.render('products');
+});
+
+router.get('/admin/requests', (req, res) => {
+    res.render('requests');
+});
 
 module.exports = router
