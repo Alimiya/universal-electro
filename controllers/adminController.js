26e89b37e2e734664b060683b1eb9b41f0b5d690
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-02-19 00:24:59 +0600

    fix to catalog and instructions for update

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -26,7 +26,7 @@ exports.createProduct = async function (req, res, next) {
     let articul = req.body.articul
 
     if (!title || !description || !price || !category || !status || !quantity || !kazniisa || !articul) {
-        fs.unlink(product_img.path, () => {})
+        fs.unlink(product_img.path, () => { })
         return res.send(r)
     }
 
@@ -43,29 +43,25 @@ exports.createProduct = async function (req, res, next) {
         await storage.upload(product_img.path, {
             gzip: true,
             metadata: metadata,
-            destination: `products/${product_img.originalname}`
+            destination: `products/${pd.id}`
         });
-        var product_image_url = `https://firebasestorage.googleapis.com/v0/b/universal-electro.appspot.com/o/products%2F${product_img.originalname}?alt=media`
+        var product_image_url = `https://firebasestorage.googleapis.com/v0/b/universal-electro.appspot.com/o/products%2F${pd.id}?alt=media`
         await fdb.collection('products').doc(pd.id).update({ product_img: product_image_url, product_id: pd.id }).then(() => {
             r['r'] = 1;
             r['product_id'] = pd.id;
             res.send(r);
-
-            fs.unlink(product_img.path, () => {
-
-            })
-        })
+            fs.unlink(product_img.path, () => {});
+        });
     }).catch((e) => {
         console.log(e);
         res.send(r);
-        fs.unlink(product_img.path, () => {
-
-        })
-    })
+        fs.unlink(product_img.path, () => {});
+    });
 }
 // POST /product/update/:id
 exports.updateProduct = async function (req, res, next) {
     let r = { r: 0 };
+    console.log(req.file)
     let product_id = req.body.product_id;
     let new_title = req.body.new_title;
     let new_description = req.body.new_description;
@@ -73,33 +69,38 @@ exports.updateProduct = async function (req, res, next) {
     let new_category = req.body.category;
     let new_status = req.body.status;
     let new_quantity = req.body.quantity;
-    let new_product_img = req.file;
+    let new_product_img = req.file; 
     let new_kazniisa = req.body.kazniisa;
     let new_articul = req.body.articul;
-
+    storage.de
 
     if (!new_title || !new_description || !new_price || !new_category || !new_status || !new_quantity || !new_kazniisa || !new_articul) {
         res.send(r);
         return;
-    }
+    }   
 
-    await fdb.collection('products').doc(product_id).update({
-        title: new_title,
-        description: new_description,
-        price: new_price,
-        category: new_category,
-        status: new_status,
-        quantity: new_quantity,
-        kazniisa: new_kazniisa,
-        articul: new_articul
-    }).then(() => {
-        r['r'] = 1;
-        r['product_id'] = product_id;
-        res.send(r);
-    }).catch((e) => {
-        console.log(e);
-        res.send(r);
-    })
+    res.send(r)
+    // if new_product_img is not undefined then 1) First delete image from storage 2)  upload image to storage and 3) then to firestore update 
+
+  
+
+    // await fdb.collection('products').doc(product_id).update({
+    //     title: new_title,
+    //     description: new_description,
+    //     price: new_price,
+    //     category: new_category,
+    //     status: new_status,
+    //     quantity: new_quantity,
+    //     kazniisa: new_kazniisa,
+    //     articul: new_articul
+    // }).then(() => {
+    //     r['r'] = 1;
+    //     r['product_id'] = product_id;
+    //     res.send(r);
+    // }).catch((e) => {
+    //     console.log(e);
+    //     res.send(r);
+    // })
 }
 
 // POST /product/delete/:id
@@ -115,19 +116,19 @@ exports.deleteProduct = async function (req, res, next) {
         r['r'] = 1;
         r['product_id'] = product_id;
         res.send(r);
-    }).catch((e)=>{
+    }).catch((e) => {
         console.log(e);
         res.send(r);
     })
 }
 
 // GET /requests
-exports.getRequests = async (req,res)=>{
+exports.getRequests = async (req, res) => {
     let data = []
 
-    try{
-        await fdb.collection('requests').get().then((requests)=>{
-            requests.forEach((request)=>{
+    try {
+        await fdb.collection('requests').get().then((requests) => {
+            requests.forEach((request) => {
                 const request_data = {
                     request_id: request.id,
                     question: request.data().question,
@@ -155,7 +156,7 @@ exports.deleteRequest = async (req, res) => {
         r['r'] = 1;
         r['request_id'] = request_id;
         res.send(r);
-    }).catch((e)=>{
+    }).catch((e) => {
         console.log(e);
         res.send(r);
     })
