d512d10c417774da427f3d42cb73c112370fdcc9
YaKartoshka <erasyl.zarubekovv@gmail.com>
2024-02-19 02:06:59 +0600

    logout added, icon modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,41 @@
 const express = require('express');
-const {generateAdminToken} = require("../middlewares/token")
+const { generateAdminToken } = require("../middlewares/token")
 const fauth = require('../firebase').fauth;
 
 exports.login = async function (req, res, next) {
-    let r = { r: 0 };
-    const {email, password} = req.body;
-    try {
-        fauth.signInWithEmailAndPassword(fauth.getAuth(), email, password).then((userCredential) => {
-          const user_id = userCredential.user.uid;
-            const token = generateAdminToken(userCredential)
-            res.cookie('admin', token, {maxAge: process.env.TOKEN_EXPIRE * 100000})
-            res.header('Authorization', `Bearer ${token}`)
-            r['r'] = 1;
-            res.send(r)
-        }, (err) => {
-            console.log(err);
-            if (err.code == 'auth/user-not-found') {
-              r['r'] = 2;
-            } else if (err.code == 'auth/wrong-password') {
-              r['r'] = 0;
-            } else if (err.code == 'auth/too-many-requests') {
-              r['r'] = 3;
-            } else if (err.code == 'auth/invalid-credential') {
-              r['r'] = 4;
-            }
-            res.send(JSON.stringify(r));
-          });
-    } catch (err) {
-        console.log(err);
-        r['r'] = 6;
-        res.send(r);
-    }
+  let r = { r: 0 };
+  const { email, password } = req.body;
+  try {
+    fauth.signInWithEmailAndPassword(fauth.getAuth(), email, password).then((userCredential) => {
+      const user_id = userCredential.user.uid;
+      const token = generateAdminToken(userCredential)
+      res.cookie('admin', token, { maxAge: process.env.TOKEN_EXPIRE * 100000 })
+      res.header('Authorization', `Bearer ${token}`)
+      r['r'] = 1;
+      res.send(r)
+    }, (err) => {
+      console.log(err);
+      if (err.code == 'auth/user-not-found') {
+        r['r'] = 2;
+      } else if (err.code == 'auth/wrong-password') {
+        r['r'] = 0;
+      } else if (err.code == 'auth/too-many-requests') {
+        r['r'] = 3;
+      } else if (err.code == 'auth/invalid-credential') {
+        r['r'] = 4;
+      }
+      res.send(JSON.stringify(r));
+    });
+  } catch (err) {
+    console.log(err);
+    r['r'] = 6;
+    res.send(r);
+  }
+}
+
+exports.logout = async function (req, res, next) {
+  let r = { r: 1 };
+  res.cookie('admin', null);
+  res.header('Authorization', null);
+  res.send(r)
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const Controller = require('../controllers/authController')
 
 router.post('/login', Controller.login);
+router.post('/logout', Controller.logout);
 
 module.exports = router
